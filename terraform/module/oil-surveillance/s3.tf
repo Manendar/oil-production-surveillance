6ce543d89eebbccd9db452e52722ec940c62e281
myadav <manen.yadav@gmail.com>
2026-07-09 14:47:56 +0100

    fix: add force_destroy to S3 and ECR for clean teardown

--- terraform/module/oil-surveillance/s3.tf
+++ terraform/module/oil-surveillance/s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "pipeline_data" {
   bucket = "${var.project_name}-${var.environment}"
+  force_destroy = true
 
   tags = {
     Name        = "${var.project_name}-${var.environment}"
